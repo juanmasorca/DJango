a7650768b45701096a39152cc3fe88e9d2c78777
juanmasorca <juanmasorca@gmail.com>
2023-03-28 18:51:02 -0500

    Update

--- myapp/views.py
+++ myapp/views.py
@@ -1,10 +1,25 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from .models import Project
 # Create your views here.
 
 
-def hello(request):
-    return HttpResponse("HelloWord")
+def index(request):
+    return HttpResponse('Index Page')
+
+
+def hello(request, username):
+    print(username)
+    return HttpResponse("Hello %s" % username)
 
 
 def about(request):
     return HttpResponse("About")
+
+
+def projects(request):
+    projects = list(Project.objects.values())
+    return JsonResponse(projects, safe=False)
+
+
+def tasks(request):
+    return HttpResponse("Tasks")
